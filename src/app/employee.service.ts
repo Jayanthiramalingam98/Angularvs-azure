8e356cfea9cfd4eb4fa767ce8279c136d3c4478c
Jayanthi Ramalingam <jayanthir@stg.com>
2022-03-17 16:11:57 +0530

    Azure Test

---
+++ src/app/employee.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  production: false
+  public baseUrl = 'http://172.16.2.46:8081/api/v1/employees';
+
+ // private baseUrl = '172.16.2.46';
+
+  constructor(private http: HttpClient) { }
+
+  getEmployee(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+  getEmployees(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createEmployee(employee: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, employee);
+  }
+
+  updateEmployee(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+
+  getEmployeesList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+}
